a7493bfea9302dab6ac3a37e64f4c8e1fac46c95
Firas Dwiyansyah <vrrases@gmail.com>
2020-04-08 12:59:57 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const Telegram = require('telegraf/telegram')
+const cron = require('node-cron');
+const axios = require('axios');
+require('dotenv').config();
+
+const telegram = new Telegram(process.env.BOT_TOKEN)
+
+console.log('Bot is Running!');
+
+const blastMessage = async (message) => {
+    const dataBot = await new Promise((resolve, reject) => {
+        // Make a request for a user with a given ID
+        axios.get('https://api.telegram.org/bot1266224642:AAF53c5DNwocn6jw5EvR0JyC2DPxHfa4YQg/getUpdates')
+            .then(function (response) {
+                // handle success
+                resolve(response.data.result);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+                reject(error);
+            });
+    });
+
+    for (const value of dataBot) {
+        if (value.message.chat.type === 'group') {
+            telegram.sendMessage(value.message.chat.id, message);
+        }
+    }
+}
+
+const cronJob = (message = "Saya adalah bot pengingat, saya ada!", hour, minute) => {
+    cron.schedule(`${minute} ${hour} * * *`, () => {
+        blastMessage(message);
+    }, {
+        scheduled: true,
+        timezone: 'Asia/Jakarta'
+    });
+}
+
+// Job 1
+cronJob("Sampurasun, good morning. Geura hudang ulah sare wae, sing semangat atuh! Ulah sampe telat nya!", 07, 00);
+// Job 2
+cronJob("15 menit deui jam asup gawe, ulah sampe poho absen asup nya!", 07, 45);
+// Job 3
+cronJob("Barudak ayeuna geus jam 5, geura isi timesheet na ulah sampe poho, absen balik ongkoh tapi kudu leuwih ti 8 jam nya! Hiji deui push jeung merge request gawean na!", 05, 00);
